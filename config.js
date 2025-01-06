136f7e63b6c2039ccc18176fa43d1b23fef3cffa
Ty Grant <74119839+ty-grant@users.noreply.github.com>
2025-01-06 12:16:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 	greetingNight: 'Good night,',
 
 	// Layout
-	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
